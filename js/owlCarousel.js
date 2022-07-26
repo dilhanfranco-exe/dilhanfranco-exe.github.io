c07e83f7425abb4c15f4135e062ceacf75d6a331
Dilhan Franco <53587310+dilhanfranco-exe@users.noreply.github.com>
2022-07-26 01:01:10 -0700

    initial test

---
+++ js/owlCarousel.js
@@ -0,0 +1,16 @@
+	$('.owl-carousel').owlCarousel({
+    items: 9,
+    loop:true,
+    margin:16,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
